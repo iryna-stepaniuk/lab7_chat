af0bb82bcb3bf83df4d93c33f8cc52e6b4b0a1d5
iryna-stepaniuk <stepanuk.ira@gmail.com>
2019-12-25 13:51:44 +0200

    fix lins

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -3,7 +3,7 @@ import {Redirect} from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
+import {Link} from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
