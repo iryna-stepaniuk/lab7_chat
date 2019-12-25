0ebe76857ce68f3b71622cbef24dc1160c059b07
iryna-stepaniuk <stepanuk.ira@gmail.com>
2019-12-25 13:00:35 +0200

    fireabse config

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,14 @@ import App from './App';
 import firebase from 'firebase'
 
 firebase.initializeApp({
-    apiKey: "AIzaSyD1voPHoDLS-wXsE5KX1p5DXOWUCKPF19U",
-    authDomain: "testproject-fcaaf.firebaseapp.com",
-    databaseURL: "https://testproject-fcaaf.firebaseio.com",
-    projectId: "testproject-fcaaf",
-    storageBucket: "testproject-fcaaf.appspot.com",
-    messagingSenderId: "12418952429",
-    appId: "1:12418952429:web:dc4d7c09460dd1ba40b0e4"
+    apiKey: "AIzaSyBfxjNjjsQKIoSHSj1ZNMIQ18HS1m0h6F4",
+    authDomain: "lab7-chat.firebaseapp.com",
+    databaseURL: "https://lab7-chat.firebaseio.com",
+    projectId: "lab7-chat",
+    storageBucket: "lab7-chat.appspot.com",
+    messagingSenderId: "698623761262",
+    appId: "1:698623761262:web:d47d37a0d755339d887f7f",
+    measurementId: "G-12FSK5YH7F"
 });
 
 export const db = firebase.firestore();
