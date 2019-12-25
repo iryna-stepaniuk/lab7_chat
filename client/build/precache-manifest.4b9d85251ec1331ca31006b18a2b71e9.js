70a97dfa130621b25929fa43ae7cb6bd7e5ef665
iryna-stepaniuk <stepanuk.ira@gmail.com>
2019-12-25 12:56:23 +0200

    build

---
+++ client/build/precache-manifest.4b9d85251ec1331ca31006b18a2b71e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34b3381cae83d76c40fcf8090783b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1704df57b38711c64c3e",
+    "url": "/static/css/main.00cde9e6.chunk.css"
+  },
+  {
+    "revision": "9a31084c31373fdcc435",
+    "url": "/static/js/2.3282388a.chunk.js"
+  },
+  {
+    "revision": "be6446f1326851a5dfb6f79c403b46c8",
+    "url": "/static/js/2.3282388a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1704df57b38711c64c3e",
+    "url": "/static/js/main.f4578d75.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
